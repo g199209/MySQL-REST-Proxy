dcfea993041898b0373cfa4df719790cc1e314b2
Gao Mingfei <g199209@gmail.com>
2022-06-05 17:33:09 +0800

    First release.
    Signed-off-by: Gao Mingfei <g199209@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # MySQL-REST-Proxy
+
+This is a simple node.js program used to provide RESTful interface for MySQL.
+
+Frontend can do CRUD operations directly by sending RESTful request.
+
+> Note: This's not safety, so do not use this method in production.
+
+## Usage
+
+- MySQL connection configure:
+Edit `mysql-config-demo.json` file and rename to `mysql-config.json`.
+
+- Install dependencies:
+```bash
+npm install
+```
+
+- Run:
+```bash
+npm start
+```
+or
+```bash
+sudo PORT=80 node app.js
+```
+
+- Build docker image:
+```
+npm run build
+```
+
+## API Doc
+
+https://www.apifox.cn/apidoc/shared-0f9baa3a-6a1e-40b6-9ae6-082b95005065/api-21444582
+
+The API is designed for [Baidu AMIS](https://github.com/baidu/amis), a front-end low-code framework.
--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 // Importing the dependencies 
-const express = require('express'); 
-const cors = require('cors'); 
-const morgan = require('morgan'); 
-const mysql = require('mysql-await');
-const fs = require('fs');
+const express = require("express"); 
+const cors = require("cors"); 
+const morgan = require("morgan"); 
+const mysql = require("mysql-await");
+const fs = require("fs");
 
 // Init MySQL connection
-const connection = mysql.createConnection(JSON.parse(fs.readFileSync('mysql-config.json')));
+const connection = mysql.createConnection(JSON.parse(fs.readFileSync("mysql-config.json")));
 
-connection.on('error', (err) => {
+connection.on("error", (err) => {
   console.error(`MySQL Connection error ${err.code}`);
 });
 
 // Defining the Express app 
 const app = express(); 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
   
 // Calling the express.json() method for parsing
 app.use(express.json()); 
@@ -23,20 +23,394 @@ app.use(express.json());
 app.use(cors()); 
   
 // Adding morgan to log HTTP requests 
-app.use(morgan('combined')); 
+app.use(morgan("combined")); 
 
-app.get('/:id', async (req, res) => { 
-  // connection.query()
-  let result = await connection.awaitQuery(`SELECT * FROM node_test WHERE id = ?`, [req.params.id]);
-  res.status(200).json({
-    id: result[0].id,
-    name: result[0].name
+//------------------------------------------------------------------------//
+
+// Common
+function handleSQLError(err, res) {
+    switch(err.code) {
+      case "ER_NO_SUCH_TABLE":
+        console.error(err.sqlMessage);
+        res.status(404).json({
+          status: err.errno,
+          msg: err.sqlMessage
+        });
+        break;
+      
+      default:
+        console.error(err);
+        res.status(500).json({
+          status: err.errno,
+          msg: err.sqlMessage
+        });
+        break;
+    }
+}
+
+function handleGenerateSQLError(err_msg, req, res) {
+    res.status(400).json({
+        status: 400,
+        msg: err_msg,
+        req: req.body
+    });
+}
+
+function initOkJson(result) {
+  return {
+    status: 0,
+    msg: result.message,
+    affected: result.affectedRows,
+    changed: result.changedRows
+  }
+}
+
+function generateWhereClause(condition_group) {
+  if (!("conjunction" in condition_group && "children" in condition_group)) {
+    return "";
+  }
+  let conjunction = condition_group.conjunction.toUpperCase();
+  if (conjunction != "AND" && conjunction != "OR") {
+    return "";
+  }
+
+  let clause = "";
+  let first_condition = true;
+  condition_group.children.forEach(condition => {
+    if (!first_condition) {
+      clause += ` ${conjunction} `;
+    } else {
+      first_condition = false;
+    }
+
+    if ("op" in condition && "left" in condition) {
+      if (!("type" in condition.left && "field" in condition.left && condition.left.type == "field")) {
+        return "";
+      }
+
+      // Check
+      switch(condition.op) {
+        case "between":
+        case "not_between":
+          if (!Array.isArray(condition.right) || condition.right.length != 2) {
+            return "";
+          }
+          break;
+
+        case "select_any_in":
+        case "select_not_any_in":
+          if (!Array.isArray(condition.right)) {
+            return "";
+          }
+          break;
+
+        case "is_empty":
+        case "is_not_empty":
+          break;
+
+        default:
+          if (!("right" in condition && !Array.isArray(condition.right))) {
+            return "";
+          }
+          break;
+      }
+
+      switch(condition.op) {
+        case "equal":
+        case "select_equals":
+          clause += `(${condition.left.field} = "${condition.right}")`;
+          break;
+
+        case "not_equal":
+        case "select_not_equals":
+          clause += `(${condition.left.field} != "${condition.right}")`;
+          break;
+
+        case "less":
+          clause += `(${condition.left.field} < "${condition.right}")`;
+          break;
+
+        case "less_or_equal":
+          clause += `(${condition.left.field} <= "${condition.right}")`;
+          break;
+
+        case "greater":
+          clause += `(${condition.left.field} > "${condition.right}")`;
+          break;
+
+        case "greater_or_equal":
+          clause += `(${condition.left.field} >= "${condition.right}")`;
+          break;
+
+        case "between":
+          clause += `(${condition.left.field} BETWEEN "${condition.right[0]}" and "${condition.right[1]}")`;
+          break;
+
+        case "not_between":
+          clause += `(${condition.left.field} NOT BETWEEN "${condition.right[0]}" and "${condition.right[1]}")`;
+          break;
+
+        case "is_empty":
+          clause += `(${condition.left.field} IS NULL)`;
+          break;
+
+        case "is_not_empty":
+          clause += `(${condition.left.field} IS NOT NULL)`;
+          break;
+
+        case "like":
+          clause += `(${condition.left.field} LIKE "${condition.right}")`;
+          break;
+
+        case "not_like":
+          clause += `(${condition.left.field} NOT LIKE "${condition.right}")`;
+          break;
+
+        case "starts_with":
+          clause += `(${condition.left.field} LIKE "${condition.right}%")`;
+          break;
+
+        case "ends_with":
+          clause += `(${condition.left.field} LIKE "%${condition.right}")`;
+          break;
+        
+        case "select_not_any_in":
+          let not_any_flag = true;
+        case "select_any_in":
+          if (not_any_flag) {
+            clause += `(${condition.left.field} NOT IN (`;
+          } else {
+            clause += `(${condition.left.field} IN (`;
+          }
+          let first_any_value = true;
+          condition.right.forEach(v => {
+            if (!first_any_value) {
+              clause += ",";
+            } else {
+              first_any_value = false;
+            }
+            clause += `"${v}"`;
+          });
+          if (first_any_value) {
+            return "";
+          }
+          clause += "))";
+          break;
+
+        default:
+          console.log(`Invalid condition op ${condition.op}`);
+          return "";
+      }
+    } else if ("conjunction" in condition) {
+      let sub_clause = generateWhereClause(condition);
+      if (sub_clause === "")
+        return "";
+      clause += `(${sub_clause})`;
+    } else {
+      return "";
+    }
   });
+  return clause;
+} 
+
+// Create
+function generateInsertSQL(req) {
+  let req_json = req.body;
+  if (!("values" in req_json)) {
+    return ["", "Do not contains 'values' field!"];
+  }  
+  let sql = `INSERT INTO ${req.params.table} SET `;
+
+  let first_col = true;
+  req_json.values.forEach(col => {
+    if (!("column" in col && "value" in col)) {
+      return ["", "Do not contains 'column' or 'value' field!"];
+    }
+    if (!first_col) {
+      sql += ',';
+    } else {
+      first_col = false;
+    }
+    sql += `${col.column}="${col.value}"`;
+  });
+  if (first_col) {
+    return ["", "'values' field is empty"];
+  }
+
+  console.log(`Generate SQL: ${sql}`);
+  return [sql, ""];
+}
+
+app.post("/:table", async (req, res) => { 
+  let [sql, err_msg] = generateInsertSQL(req);
+  if (!sql || sql === "") {
+    return handleGenerateSQLError(err_msg, req, res);
+  }
+
+  try {
+    let result = await connection.awaitQuery(sql);
+    console.log(result);
+    res.status(200).json(initOkJson(result));
+  } catch (err) {
+    handleSQLError(err, res);
+  }
 });
+
+// Update
+function generateUpdateSQL(req) {
+  let req_json = req.body;
+  if (!("values" in req_json)) {
+    return ["", "Do not contains 'values' field!"];
+  }  
+  let sql = `UPDATE ${req.params.table} SET `;
+
+  let first_col = true;
+  req_json.values.forEach(col => {
+    if (!("column" in col && "value" in col)) {
+      return ["", "Do not contains 'column' or 'value' field!"];
+    }
+    if (!first_col) {
+      sql += ',';
+    } else {
+      first_col = false;
+    }
+    sql += `${col.column}="${col.value}"`;
+  });
+  if (first_col) {
+    return ["", "'values' field is empty"];
+  }
+
+  if (req_json.conditions) {
+    let where_clause = generateWhereClause(req_json.conditions);
+
+    if (where_clause === "") {
+      return ["", "Where conditions error"];
+    }
+    sql += ` WHERE (${where_clause})`;
+  }
+
+  console.log(`Generate SQL: ${sql}`);
+  return [sql, ""];
+}
+
+app.put("/:table", async (req, res) => { 
+  let [sql, err_msg] = generateUpdateSQL(req);
+  if (!sql || sql === "") {
+    return handleGenerateSQLError(err_msg, req, res);
+  }
+
+  try {
+    let result = await connection.awaitQuery(sql);
+    console.log(result);
+    res.status(200).json(initOkJson(result));
+  } catch (err) {
+    handleSQLError(err, res);
+  }
+});
+
+// Delete
+function generateDeleteSQL(req) {
+  let req_json = req.body;
+  let sql = `DELETE FROM ${req.params.table}`;
+
+  if (req_json.conditions) {
+    let where_clause = generateWhereClause(req_json.conditions);
+
+    if (where_clause === "") {
+      return ["", "Where conditions error"];
+    }
+    sql += ` WHERE (${where_clause})`;
+  } else {
+    // Do not allow delete whole table!
+    console.warn("Do not allow delete whole table!");
+    return ["", "Do not allow delete whole table!"];
+  }
+
+  console.log(`Generate SQL: ${sql}`);
+  return [sql, ""];
+}
+
+app.delete("/:table", async (req, res) => { 
+  let [sql, err_msg] = generateDeleteSQL(req);
+  if (!sql || sql === "") {
+    return handleGenerateSQLError(err_msg, req, res);
+  }
+
+  try {
+    let result = await connection.awaitQuery(sql);
+    console.log(result);
+    res.status(200).json(initOkJson(result));
+  } catch (err) {
+    handleSQLError(err, res);
+  }
+});
+
+// Select
+function generateSelectSQL(req) {
+  let req_json = req.body;
+  let sql = `SELECT `;
+  if (Array.isArray(req_json.columns)) {
+    let first_col = true;
+    req_json.columns.forEach(col => {
+      if (!first_col) {
+        sql += ',';
+      } else {
+        first_col = false;
+      }
+      sql += `${col}`;
+    });
+    if (first_col) {
+      return ["", "Has columns but empty!"];
+    }
+  } else {
+    sql += '*';
+  }  
+  sql += ` FROM ${req.params.table}`;
+
+  if (req_json.conditions) {
+    let where_clause = generateWhereClause(req_json.conditions);
+
+    if (where_clause === "") {
+      return ["", "Where conditions error"];
+    }
+    sql += ` WHERE (${where_clause})`;
+  } 
+  console.log(`Generate SQL: ${sql}`);
+  return [sql, ""];
+}
+
+app.get("/:table", async (req, res) => { 
+  let [sql, err_msg] = generateSelectSQL(req);
+  if (!sql || sql === "") {
+    return handleGenerateSQLError(err_msg, req, res);
+  }
+
+  try {
+    let result = await connection.awaitQuery(sql);
+    // console.log(result);
+    j = {
+      status: 0,
+      msg: "Success",
+      data: {
+        items: [],
+        total: result.length
+      }
+    };
+    result.forEach(row => {
+      j.data.items.push(row);
+    })
+
+    res.status(200).json(j);
+  } catch (err) {
+    handleSQLError(err, res);
+  }
+});
+
+//------------------------------------------------------------------------//
   
 // starting the server 
 app.listen(PORT, (err) => { 
   if (err)
     console.log(err);
-  console.log('Server listening on port', PORT);
+  console.log("Server listening on port", PORT);
 }); 
